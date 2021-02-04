54ade42e99f520ea759cae290d67e0899ec02051
Ubuntu <zjing@umail.ucsb.edu>
2021-02-04 19:44:48 +0000

    Updated...

--- diffexpsets.py
+++ diffexpsets.py
@@ -28,8 +28,18 @@ def main():
     for k, v in inv_map.items():
         mean_dfs.append(assay.loc[:, v].mean(axis=1))
         std_dfs.append(assay.loc[:, v].std(axis=1))
+
     mean_df = pd.concat(mean_dfs, axis=1)
     std_df = pd.concat(std_dfs, axis=1)
+
+    mean_rest_dfs = []
+    std_rest_dfs = []
+    for k, v in inv_map.items():
+        rest_v = list(set(assay.Index.values()).difference(set(v)))
+        mean_rest_dfs.append(assay.loc[:, rest_v].mean(axis=1))
+        std_rest_dfs.append(assay.loc[:, rest_v].std(axis=1))
+    mean_rest_df = pd.concat(mean_rest_dfs, axis=1)
+    std_rest_df = pd.concat(std_rest_dfs, axis=1)
 
     zscore_dfs = []
     colnames = []
@@ -38,13 +48,17 @@ def main():
             if coli != colj:
                 zscore_dfs.append(((mean_df[coli]-mean_df[colj])/(std_df[colj]+1.0/max_zscore)).fillna(0).clip(-max_zscore, max_zscore))
                 colnames.append("{} vs {}".format(coli, colj)) 
+    for coli in mean_df:
+        zscore_dfs.append(((mean_df[coli]-mean_rest_df[colj])/(std_rest_df[colj]+1.0/max_zscore)).fillna(0).clip(-max_zscore, max_zscore))
+        colnames.append("{} vs rest".format(coli)) 
+
     zscore_df = pd.concat(zscore_dfs, axis=1)
     zscore_df.columns = colnames
     norms_df = zscore_df.apply(np.linalg.norm, axis=1)
     colsmatching = norms_df.T[(norms_df.T >= min_zscore)].index.values
     return_df = zscore_df.T[colsmatching]
     gn.export_statically(gn.assay_from_pandas(return_df), 'Differential expression sets')
-    gn.export(return_df.to_csv(), 'differential_gene_sets.csv', kind='raw', meta=None, raw=True)
+    gn.export(return_df.T.to_csv(), 'differential_gene_sets.csv', kind='raw', meta=None, raw=True)
 
     toc = time.perf_counter()
     time_passed = round(toc - tic, 2)
