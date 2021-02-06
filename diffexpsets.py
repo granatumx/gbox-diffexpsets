d7ea1866232403aa6df0ffa23a03c5aa191e22dc
Ubuntu <zjing@umail.ucsb.edu>
2021-02-06 15:48:43 +0000

    Updated...

--- diffexpsets.py
+++ diffexpsets.py
@@ -80,7 +80,6 @@ def main():
                 mean_diff_overlap_low_high = (low_mean_df[coli]-high_mean_df[colj])
                 mean_diff_overlap_high_low = (high_mean_df[coli]-low_mean_df[colj])
                 mean_df = mean_diff_overlap_low_high.combine(mean_diff_overlap_high_low, range_check).to_frame()
-                mean_df.columns = cols
 
                 zscore_dfs.append(((mean_df[coli]-mean_df[colj])/(std_df[colj])).fillna(0).clip(-max_zscore, max_zscore))
                 colnames.append("{} vs {}".format(coli, colj)) 
