abf437d4539b15af2957b96e8550903e7792bc66
Ubuntu <zjing@umail.ucsb.edu>
2021-02-06 15:46:34 +0000

    Updated...

--- diffexpsets.py
+++ diffexpsets.py
@@ -79,11 +79,8 @@ def main():
                 # Fetch most realistic mean comparison set, what is smallest difference between two ranges
                 mean_diff_overlap_low_high = (low_mean_df[coli]-high_mean_df[colj])
                 mean_diff_overlap_high_low = (high_mean_df[coli]-low_mean_df[colj])
-                mean_df = mean_diff_overlap_low_high.combine(mean_diff_overlap_high_low, range_check)
-                print("Combined {} with {}".format(coli, colj), flush=True)
-                print(type(mean_df), flush=True)
+                mean_df = mean_diff_overlap_low_high.combine(mean_diff_overlap_high_low, range_check).to_frame()
                 mean_df.columns = cols
-                print(mean_df, flush=True)
 
                 zscore_dfs.append(((mean_df[coli]-mean_df[colj])/(std_df[colj])).fillna(0).clip(-max_zscore, max_zscore))
                 colnames.append("{} vs {}".format(coli, colj)) 
